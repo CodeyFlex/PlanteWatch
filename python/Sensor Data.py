fc2197b1bcf0963d193cf1afea229dbe59e23be6
younas kronborg abachri <younas1996@live.dk>
2020-12-10 12:13:53 +0100

    added 3 randoms data points

--- python/Sensor Data.py
+++ python/Sensor Data.py
@@ -1,3 +1,16 @@
-from sense_hat import SenseHat
+ sense_hat = SenseHat
+# import time
+import random
+ sense = SenseHat()
 
-sense = SenseHat()
+
+random.randint(0,9)
+def temp(temp):
+
+    return random.randint(20,30)
+
+def humid():
+    return random.randint(30,80)
+
+def light():
+    return random.randint(30, 150)
